3cc033f8709a48c4b79d0ecd37615e499e737131
Aika Okemoto <aika2004oke@gmail.com>
2020-07-18 18:55:51 +0900

    Initial Commit

---
+++ TechPad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechPad
+//
+//  Created by 桶本あいか on 2020/07/18.
+//  Copyright © 2020 Okemoto.Aika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
